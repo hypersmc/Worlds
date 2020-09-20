ea9e6cf9cf3739c37ca5deb127feaf86c7aced28
hypersmc <mhypers@gmail.com>
2020-09-20 21:04:12 +0200

    yest

--- src/main/java/me/hypersmc/jumpwatch/worlds/GUI/MainGuiUser.java
+++ src/main/java/me/hypersmc/jumpwatch/worlds/GUI/MainGuiUser.java
@@ -1,20 +1,34 @@
 package me.hypersmc.jumpwatch.worlds.GUI;
 
+import me.mattstudios.mfgui.gui.components.ItemBuilder;
 import me.mattstudios.mfgui.gui.guis.Gui;
 import me.mattstudios.mfgui.gui.guis.GuiItem;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 
 public class MainGuiUser implements Listener {
-    Gui gui = new Gui(3, "yeet");
-    GuiItem guiItem = new GuiItem(new ItemStack(Material.STONE));
+    //Gui's
+    Gui gui = new Gui(6, "yeet");
+    Gui gui2 = new Gui(3, "yeet2");
+    //Fillitem
+    GuiItem Fillitem = ItemBuilder.from(Material.GRAY_STAINED_GLASS_PANE).setName("").asGuiItem(event -> {
+        // Handle your click action here
+        event.setCancelled(true);
+    });
 
+    //Gui items for main
+    GuiItem guiItem = ItemBuilder.from(Material.STONE).setName("dw").asGuiItem(event -> {
+        // Handle your click action here
+        event.setCancelled(true);
+        gui2.open(event.getWhoClicked());
+    });
+    //Gui items for
 
     public void maingui(Player player) {
         gui.open(player);
-        gui.setItem(1, guiItem);
+        gui.setItem(0-13, Fillitem);
+        gui.setItem(14, guiItem);
     }
 
 
