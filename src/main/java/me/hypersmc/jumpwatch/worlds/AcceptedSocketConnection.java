0011a42f4b46dd845c18b0e149bc7265c7a41b02
JumpWatch <mhypers@gmail.com>
2020-09-28 15:18:51 +0200

    stuff to fix

--- src/main/java/me/hypersmc/jumpwatch/worlds/AcceptedSocketConnection.java
+++ src/main/java/me/hypersmc/jumpwatch/worlds/AcceptedSocketConnection.java
@@ -10,10 +10,10 @@
  */
 
 package me.hypersmc.jumpwatch.worlds;
+
 import java.io.*;
 import java.net.Socket;
 
-import org.bukkit.ChatColor;
 public class AcceptedSocketConnection extends Thread{
     Socket sock;
     Main plugin;
@@ -21,13 +21,17 @@ public class AcceptedSocketConnection extends Thread{
         // TODO Auto-generated constructor stub
         this.sock = sock;
         this.plugin = plugin;
+
     }
     @Override
     public void run() {
         try {
+            BufferedOutputStream dataOut = null;
             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+            dataOut = new BufferedOutputStream(sock.getOutputStream());
 
+            String contentMimeType = "text/html";
 
             String s;
             int counterr = 0, contentLength = 0;
@@ -50,31 +54,47 @@ public class AcceptedSocketConnection extends Thread{
             for(int i = 0; i < contentLength; i++){
                 finalString += (char) in.read();
             }
-            plugin.getLogger().info(finalString);//>Request from client is logged on console
+
             //This section is the response to the clients request, the web page:
             out.write("HTTP/1.0 200 OK\r\n");
             out.write("Date: Fri, 31 Dec 1999 23:59:59 GMT\r\n");
             out.write("Server: Apache/0.8.4\r\n");
             out.write("Content-Type: text/html\r\n");
-            out.write("Content-Length: " + 36 + "\r\n");
             out.write("Expires: Sat, 01 Jan 2000 00:59:59 GMT\r\n");
             out.write("Last-modified: Fri, 09 Aug 1996 14:21:40 GMT\r\n");
             out.write("\r\n");
             //Content of returned html page is here:
-            File file = new File("plugins/Worlds/web/index.html");
-            File file2 = new File("plugins/Worlds/web/index.php");
+            File file = new File( plugin.getDataFolder() + "/web/index.html");
+            File file2 = new File(plugin.getDataFolder() + "/web/index.php");
 
             if (file.isFile()) {
-                BufferedReader in2 = new BufferedReader(new InputStreamReader(file.toURL().openStream()));
-                out.write(in2.readLine());
+                if (plugin.getConfig().getBoolean("UseHtml")) {
+                    BufferedReader reader;
+                    reader = new BufferedReader(new FileReader(file));
+                    String line = reader.readLine();
+                    while (line != null) {
+                        out.write(line);
+                        line = reader.readLine();
+                    }
+                }
+
 
 
             } else if (file2.isFile()){
-                BufferedReader in3 = new BufferedReader(new InputStreamReader(file2.toURL().openStream()));
-                out.write(in3.readLine());
+                if (plugin.getConfig().getBoolean("UsePHP")) {
+                    BufferedReader reader2;
+                    reader2 = new BufferedReader(new FileReader(file2));
+                    String line2 = reader2.readLine();
+                    while (line2 != null) {
+                        out.write(line2);
+                        line2 = reader2.readLine();
+                    }
+                }
+
             }else {
                 out.write("<h1>Failed</h1>");
                 out.write("<br><h2>Missing index.php or index.html</h2>");
+
             }
 
 
